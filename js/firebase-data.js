9d8217c697bdeb694633a740dd85595ce173da0e
zerokung14123 <zerokung14123@gmail.com>
2026-07-12 14:43:06 +0700

    fix: point API_BASE to production Render backend host in production

--- js/firebase-data.js
+++ js/firebase-data.js
@@ -4,7 +4,7 @@
 
 const API_BASE = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' 
   ? 'http://localhost:5000/api' 
-  : '/api';
+  : 'https://oracatapi.onrender.com/api';
 
 const state = {
   user: null,
